1eba90d09ffcd86491d7b71f155605f54f1b703e
Austin Cumberlander <austincumberlander@gmail.com>
2018-09-05 16:10:09 -0500

    created and linked js file. added language objects.

---
+++ main.js
@@ -0,0 +1,25 @@
+const spanish = {
+    "food": "comida",
+    "is": "es",
+    "my": "mi",
+    "only":"único",
+    "true": "verdadero",
+    "love": "amor"
+} ;
+const german = {
+    "food": "Essen",
+    "is": "ist",
+    "my": "meine",
+    "only":"einzige",
+    "true": "wahre",
+    "love": "Leibe"
+} ;
+const italian = {
+    "food": "il cibo",
+    "is": "è",
+    "my": "il mio",
+    "only": "unico",
+    "true": "vero",
+    "love": "amore"
+} ;
+
